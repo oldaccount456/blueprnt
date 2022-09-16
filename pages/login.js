29036a271a247b7420ae44f90f42e6f896a345c5
Roan John <roanj200@gmail.com>
2022-09-16 17:58:50 +0100

    Refactor login page to take advantage of the re-usable components

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,9 @@
 import Layout from '@/components/Layout';
 import AccountPrompt from '@/components/Form/Prompt';
-import InputField from '@/components/Form/InputField'
+import InputField from '@/components/Form/InputField';
+import {UsernameField, PasswordField, CaptchaField} from '@/components/Form/InputField/Types';
 import SubmitButton from '@/components/Form/SubmitButton';
 import FormStatus from '@/components/Form/FormStatus';
-
-import parseBoolFromStr from '@/utils/envParser';
 
 import React from 'react';
 import Axios from 'axios';
@@ -14,25 +13,20 @@ import {
 } from 'react-bootstrap';
 
 import Cookies from 'js-cookie';
-import HCaptcha from '@hcaptcha/react-hcaptcha';
 
 export default class Login extends React.Component{
     constructor(props){
         super(props);
 
         this.state = {
-            username: '',
-            password: '',
-            hcaptchaToken: '',
-            hcaptchaSize: 'normal',
             successMessage: '',
             errorMessage: '',
             processing: false,
         }
 
-        this.usernameField = React.createRef();
-        this.passwordField = React.createRef();
-        this.captchaRef = React.createRef();
+        this.usernameComponent = React.createRef();
+        this.passwordComponent = React.createRef();
+        this.captchaComponent = React.createRef();
     }
 
     handleErrorPopUp(errorMessage){
@@ -50,41 +44,32 @@ export default class Login extends React.Component{
         });
     }
 
-    componentDidMount() {
-        if(window.innerWidth < 390){
-            this.setState({
-                hcaptchaSize: 'compact'
-            })
-        }   
+    validateComponent(component, handleHighlight=true){
+        const validateUsername = component.validate();
+        if(!validateUsername.success){
+            this.handleErrorPopUp(validateUsername.message);
+            if(handleHighlight) component.highlight();
+            return false;
+        }
+
+        if(handleHighlight) component.unhighlight();
+        return true;
     }
 
     async login(e){
         e.preventDefault();
-        if(!this.state.username){
-            this.usernameField.current.style.borderColor = 'red';
-            return this.handleErrorPopUp('You must enter a username');
-        }
-        else if(this.state.username.length >= 30){
-            this.usernameField.current.style.borderColor = 'red';
-            return this.handleErrorPopUp("Usernames are below 30 characters");
-        }
-        this.usernameField.current.style.borderColor = '';
+        const usernameComponent = this.usernameComponent.current;
+        const passwordComponent = this.passwordComponent.current;
+        const captchaComponent = this.captchaComponent.current;
 
-        if(!this.state.password){
-            this.passwordField.current.style.borderColor = 'red';
-            return this.handleErrorPopUp('You must enter a password');
-        }
-        else if(this.state.password.length > 255){
-            this.passwordField.current.style.borderColor = 'red';
-            return this.handleErrorPopUp('Passwords are below 255 characters');
-        }
-        this.passwordField.current.style.borderColor = '';
+        const usernameValidation = this.validateComponent(usernameComponent);
+        if(!usernameValidation) return;
 
-        if(parseBoolFromStr(process.env.NEXT_PUBLIC_USING_HCAPTCHA)){
-            if(!this.state.hcaptchaToken){
-                return this.handleErrorPopUp("Please fill out the captcha to verify you are not a bot")
-            }
-        }
+        const passwordValidation = this.validateComponent(passwordComponent);
+        if(!passwordValidation) return;
+
+        const captchaValidation = this.validateComponent(captchaComponent, false);
+        if(!captchaValidation) return;
 
         this.setState({
             processing: true
@@ -92,9 +77,9 @@ export default class Login extends React.Component{
 
         try{
             const loginReq = await Axios.post('/api/account/login', {
-                'username': this.state.username,
-                'password': this.state.password,
-                'hcaptchaToken': this.state.hcaptchaToken,
+                'username': usernameComponent.state.value,
+                'password': passwordComponent.state.value,
+                'hcaptchaToken': captchaComponent.state.hcaptchaToken,
             });
             this.setState({
                 processing: false
@@ -108,13 +93,7 @@ export default class Login extends React.Component{
         }
         catch(err){
             console.log(err)
-            if(parseBoolFromStr(process.env.NEXT_PUBLIC_USING_HCAPTCHA)){
-                this.captchaRef.current.resetCaptcha();
-                this.setState({
-                    hcaptchaToken: ''
-                });
-            }
-
+            captchaComponent.resetCaptcha();
             let errorMessage = (!err.response.data.message || err.response.data.message == "") ?  "There was an error, please contact an admin for more." : err.response.data.message;
             if(Number(err.response.status) === 429){
                 errorMessage = err.response.data
@@ -127,29 +106,6 @@ export default class Login extends React.Component{
 
     }
 
-    handleCaptcha(token, ekey){
-        this.setState({
-            hcaptchaToken: token
-        })
-    }
-
-    updateField(e){
-        switch(e.target.id){
-            case "username":
-                this.setState({
-                    username: e.target.value
-                });
-                break;
-            case "password":
-                this.setState({
-                    password: e.target.value
-                });
-                break;
-            default:
-                console.log('Unknown field given')
-        }
-    }
-
     render(){
         return (
             <>
@@ -158,23 +114,13 @@ export default class Login extends React.Component{
                         <Form onSubmit={this.login.bind(this)}>
                             <FormStatus processing={this.state.processing} errorMessage={this.state.errorMessage} successMessage={this.state.successMessage}/>
                             <InputField id='username'>
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control ref={this.usernameField} value={this.state.username} onChange={this.updateField.bind(this)}  type="text"  placeholder="Enter username" />
+                                <UsernameField ref={this.usernameComponent} fieldName={'Username'} />
                             </InputField>
                             <InputField id='password'>
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control ref={this.passwordField} value={this.state.password} onChange={this.updateField.bind(this)}  type="password"  placeholder="Enter password" />
+                                <PasswordField ref={this.passwordComponent} fieldName={'Password'} />
                             </InputField>
                             <InputField id='hcaptcha'>
-                                {parseBoolFromStr(process.env.NEXT_PUBLIC_USING_HCAPTCHA) ? (
-                                <HCaptcha
-                                        size={this.state.hcaptchaSize}
-                                        sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY}
-                                        onVerify={(token,ekey) => this.handleCaptcha(token, ekey)}
-                                        ref={this.captchaRef}
-                                    />
-                                ) : (null)}
-                                
+                                <CaptchaField ref={this.captchaComponent}/>
                             </InputField>
                             <SubmitButton action={this.login.bind(this)} actionText='Login'/>   
                         </Form>
