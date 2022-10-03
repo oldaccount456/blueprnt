0cfc66c6dc8342e0c71a4c66f5c32d49ecf3755f
Roan John <roanj200@gmail.com>
2022-10-03 16:21:56 +0100

    Refactor verification module to have a neater way of processing emails

--- utils/verification.js
+++ utils/verification.js
@@ -3,52 +3,98 @@ const {sendEmail} = require('@/lib/aws');
 const getEmailContent = require('@/utils/getEmailContent').default;
 const getIpDetails = require('@/utils/getIpDetails').default;
 
-module.exports.createVerificationRequest = async (userQuery, verificationAction, data) => {
-    const verificationCode = await getUniqueVerificationCode();
-    switch(verificationAction){
-        case "verifyLoginRequest":
-            const verifyLoginContent = await getEmailContent('verify-login.html');
-            const locationDetails = await getIpDetails(data); 
-            const editedVerifyLoginContent = verifyLoginContent
-            .replace('{USERNAME}', userQuery.username)
-            .replace('{IP_ADDRESS}', data)
-            .replace('{LOCATION}', locationDetails)
-            .replace('{DOMAIN_ENDPOINT}', process.env.DOMAIN_ENDPOINT)
-            .replace('{CODE_HERE}', verificationCode);
+const emailActions = {
+    verifyLoginRequest: {
+        send: async (userQuery, content, _) => {
             await sendEmail({
                 'from': process.env.SYSTEM_EMAIL_ADDRESS,
                 'to': userQuery.email,
                 'subject': 'Verify BluePrnt login from new location',
-                'html': editedVerifyLoginContent
+                'html': content
             });
-            break;
-        case "verifyEmailRequest":
-            const updateEmailContent = await getEmailContent('update-email.html');
-            const editedUpdateEmailContent = updateEmailContent
-            .replace('{CODE_HERE}', verificationCode);
+        },
+        getTemplateData: async (userQuery, verificationCode, data) => {
+            const locationDetails = await getIpDetails(data); 
+            return {
+                templateVariables: {
+                    '{USERNAME}': userQuery.username,
+                    '{IP_ADDRESS}': data,
+                    '{LOCATION}': locationDetails,
+                    '{DOMAIN_ENDPOINT}': process.env.DOMAIN_ENDPOINT,
+                    '{CODE_HERE}': verificationCode,
+                },
+                templateName: 'verify-login.html'
+            };
+        },
+    },
+
+    verifyRecoveryRequest: {
+        send: async (userQuery, content, _) => {
+            await sendEmail({
+                'from': process.env.SYSTEM_EMAIL_ADDRESS,
+                'to': userQuery.email,
+                'subject': `Password Reset Request for BluePrnt`,
+                'html': content
+            });
+        },
+        getTemplateData: async (userQuery, verificationCode, _) => {
+            return {
+                templateVariables: {
+                    '{CODE_HERE}': verificationCode,
+                    '{USERNAME}': userQuery.username,
+                    '{DOMAIN_ENDPOINT}': process.env.DOMAIN_ENDPOINT,
+                },
+                templateName: 'recover-account.html',
+            };
+        },
+    },
+    verifyEmailRequest: {
+        send: async (userQuery, content, verificationCode) => {
             await sendEmail({
                 'from': process.env.SYSTEM_EMAIL_ADDRESS,
                 'to': userQuery.email,
                 'subject': `Your BluePrnt email verification code is: ${verificationCode}`,
-                'html': editedUpdateEmailContent
+                'html': content
             });
-            break;
-        case "verifyDeletionRequest":
-            const deleteAccountContent = await getEmailContent('delete-account.html');
-            const editedDeleteAccountContent = deleteAccountContent
-            .replace('{USERNAME}', userQuery.username)
-            .replace('{DOMAIN_ENDPOINT}', process.env.DOMAIN_ENDPOINT)
-            .replace('{CODE_HERE}', verificationCode);
+        },
+        getTemplateData: async (_, verificationCode, __) => {
+            return {
+                templateVariables: {
+                    '{CODE_HERE}': verificationCode,
+                },
+                templateName: 'update-email.html',
+            };
+        },
+    },
+
+    verifyDeletionRequest: {
+        send: async (userQuery, content, _) => {
             await sendEmail({
                 'from': process.env.SYSTEM_EMAIL_ADDRESS,
                 'to': userQuery.email,
                 'subject': `Verify your BluePrnt account deletion request`,
-                'html': editedDeleteAccountContent
+                'html': content
             });
-            break;
-        default: 
-            console.log("Unknown action given");
-    }
+        },
+        getTemplateData: async (userQuery, verificationCode, _) => {
+            return {
+                templateVariables: {
+                    '{USERNAME}': userQuery.username,
+                    '{DOMAIN_ENDPOINT}': process.env.DOMAIN_ENDPOINT,
+                    '{CODE_HERE}': verificationCode,
+                },
+                templateName: 'delete-account.html',
+            };
+        },
+    },
+}
+
+module.exports.createVerificationRequest = async (userQuery, verificationAction, data) => {
+    const verificationCode = await getUniqueVerificationCode();
+    const emailAction = emailActions[verificationAction];
+    const templateData = await emailAction.getTemplateData(userQuery, verificationCode, data);
+    const emailContent = await renderEmailTemplate(templateData);
+    await emailAction.send(userQuery, emailContent, verificationCode);
     await verifyRequest.create({
         id: null,
         account_id: userQuery.id,
@@ -68,4 +114,12 @@ const getUniqueVerificationCode = async () => {
         uniqueVerificationCode = Math.random().toString(26).slice(2);
     }
     return uniqueVerificationCode;
+}
+
+const renderEmailTemplate = async (templateData) => {
+    let emailContent = await getEmailContent(templateData.templateName);
+    for(const variable in templateData.templateVariables){
+        emailContent = emailContent.replace(variable, templateData.templateVariables[variable])
+    }
+    return emailContent;
 }
