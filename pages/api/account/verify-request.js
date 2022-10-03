9d6a8c17faed50530bef8fcdc5ac5b03cfd013e0
Roan John <roanj200@gmail.com>
2022-10-03 16:22:29 +0100

    Refactor the verification api route to handle actions more neatly

--- pages/api/account/verify-request.js
+++ pages/api/account/verify-request.js
@@ -1,7 +1,9 @@
-const {createToken} = require('@/lib/authentication');
+const bcrypt = require('bcryptjs');
+const {createToken, destroyToken} = require('@/lib/authentication');
 const {verifyRequest, account} = require('@/lib/database');
 const botChecks = require('@/lib/botChecks').default;
-const {validateStr} = require('@/utils/validator');
+const {validateStr, validatePassword} = require('@/utils/validator');
+const getIp = require('@/utils/getIp').default;
 
 export default async function verifyRequest_(req, res) {
     if(req.method === 'POST'){ 
@@ -54,42 +56,77 @@ export default async function verifyRequest_(req, res) {
             });
         }
         
-        await verifyRequest.destroy({
-            where: {
-                id: verificationIdQuery.id
-            }
-        });
 
-        switch(verificationIdQuery.verification_action){
-            case "verifyLoginRequest":
+
+        const verificationActions = {
+            verifyLoginRequest: async () => {
                 const token = await createToken(userQuery, verificationIdQuery.given_data);
-                return res.send(JSON.stringify({
-                    message: '',
+                return {
                     successful: true,
+                    message: '',
                     token: token
-                }));
-            case "verifyEmailRequest":
-                await account.update({ email: verificationIdQuery.given_data }, { where: { id: userQuery.id }});
-                return res.send(JSON.stringify({
-                    message: '',
-                    successful: true,
-                }));
-            case "verifyDeletionRequest":
-                await account.destroy({
-                    where: {
-                        id: userQuery.id
+                }
+            },
+            verifyRecoveryRequest: async () => {
+                if(!validateStr(req.body.password)){
+                    return {
+                        successful: false, 
+                        statusCode: 400,
+                        message: 'You sent an invalid type of request, please provide a valid string for the password'
                     }
-                });
-                return res.send(JSON.stringify({
-                    message: '',
-                    successful: true,
-                }));
+                }
+                const isPasswordValidated = validatePassword(req.body.password, true);
+                if(!isPasswordValidated.success){
+                    return {
+                        successful: false, 
+                        statusCode: 400,
+                        message: `You sent an invalid type of request, ${isPasswordValidated.message}`
+                    }
+                }
 
-            default:
-                return res.status(500).json({
-                    message: 'An unexpected error occurred, please contact an administrator for more support. [1]',
-                    successful: false
-                });
+                const newPasswordHash = await bcrypt.hash(req.body.password, 12);
+                await account.update({ password: newPasswordHash }, { where: { id: userQuery.id }});
+                await destroyToken(userQuery);
+                const ipAddress = getIp(req);
+                const token = await createToken(userQuery, ipAddress);
+                return {
+                    successful: true,
+                    message: '',
+                    token: token
+                }
+            },
+
+            verifyEmailRequest: async () => {
+                await account.update({ email: verificationIdQuery.given_data }, { where: { id: userQuery.id }});
+                return {
+                    successful: true,
+                    message: '',
+                }
+            },
+            
+            verifyDeletionRequest: async () => {
+                await account.destroy({where: {id: userQuery.id}});
+                return {
+                    successful: true,
+                    message: '',
+                }
+            }
+        }
+        const action = verificationActions[verificationIdQuery.verification_action];
+        const result = await action();
+        if(result.successful){
+            await verifyRequest.destroy({
+                where: {
+                    id: verificationIdQuery.id
+                }
+            });
+            return res.send(JSON.stringify(result));
+        }
+        else{
+            return res.status(result.statusCode).json({
+                message: result.message,
+                successful: false
+            }); 
         }
     }
     return res.status(400).json({
