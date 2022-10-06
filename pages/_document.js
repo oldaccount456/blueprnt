d9be6f37ce53999bf7e1bb0254dd0061288469f4
Roan John <roanj200@gmail.com>
2022-10-06 22:49:15 +0100

    Implement fontawesome to display an encryption icon

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document(){
+    return (
+        <Html>
+            <Head>
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
